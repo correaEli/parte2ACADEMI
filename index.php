c0932994b297cff7cd04e1c7735e05452ee0b46a
GianCardona <gian.cardona@upr.edu>
2018-02-08 16:39:57 -0500

    Actualizacion del messenger

--- index.php
+++ index.php
@@ -64,6 +64,8 @@ if ($action == 'login') {
                 $loggedProfile=$i;
                 include './parts/profile.php';
         }
+} else if ($action == 'goProfile') {
+            include './parts/messages.php';
 } else if ($action == 'goRegister') {
                 include './parts/Register.php';
 } else {
