c18d5b881bf542da36517ce0bed4ae12ae15e54c
GianCardona <gian.cardona@upr.edu>
2018-02-13 22:19:18 -0500

    added copyright and image

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,8 @@
 </div>
 <div class="footer">
+        <div class="col-sm-3">
+          <h5 class="COPYRIGHT">© 2018 ELI CORREA Y GIAN CARDONA</h5>
+        </div>
   <div class="fa"><a href="http://www.facebook.com"><img src="Components/pictures/logo1.png" width="88" height="88" alt="Facebook"/></a></div>
     <div class="in"><a href="http://www.Instagram.com"><img src="Components/pictures/logo2.png" width="88" height="86" alt="Instagram"/></a></div>
       <div class="you"><a href="http://www.youtube.com"><img src="Components/pictures/logo3.png" width="88" height="86" alt="Youtube"/></a></div>
